2750d8d9a689a9ebeb26b6d4ee259a7c810def78
ShanShan <zishan_lee@mymail.sutd.edu.sg>
2015-11-21 16:58:54 +0800

    Load & export data in json
    Tried on bookstore.Customer table.

--- documents/DatabaseDjangocopy/mysite/bookstore/models.py
+++ documents/DatabaseDjangocopy/mysite/bookstore/models.py
@@ -51,7 +51,7 @@ class Opinion(models.Model):
     customer = models.ForeignKey(Customer)
     book = models.ForeignKey(Book)
     score = models.IntegerField(validators = [validators.MinValueValidator(0, "Score must be >= 0."), validators.MaxValueValidator(10, "Score must be <= 10.")], default = 0)
-    txt = models.TextField(default = "")
+    txt = models.TextField(default = "A")
 
     class Meta:
         unique_together = ("customer", "book")
